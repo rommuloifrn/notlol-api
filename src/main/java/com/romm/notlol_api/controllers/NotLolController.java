004089375284e46a00364cab0050cd9ab58b8776
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-11-03 02:55:27 -0300

    docs: OpenAPI texts

--- src/main/java/com/romm/notlol_api/controllers/NotLolController.java
+++ src/main/java/com/romm/notlol_api/controllers/NotLolController.java
@@ -19,7 +19,7 @@ public class NotLolController {
 
     @GetMapping @Operation(summary = "Greets you.")
     public String getMethodName() {
-        return new String("Hi. My name is roms and this is an API that returns a players not-playing-lol streak. I hope you have some use to this.");
+        return new String("Hi. My name is roms and this is an API that returns a players not-playing-lol streak. Try /docs or /docs-json.");
     }
     
     @Operation(summary = "Returns the number of days since the player's last match.")
