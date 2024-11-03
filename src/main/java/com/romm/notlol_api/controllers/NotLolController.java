f650290122d30624a32091abebb598ee360555b4
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-11-03 02:44:48 -0300

    feat/refactor: remove HelloService/Controller, remove not essential endpoints, rename PlayerController to NotLolController

---
+++ src/main/java/com/romm/notlol_api/controllers/NotLolController.java
@@ -0,0 +1,31 @@
+package com.romm.notlol_api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.romm.notlol_api.DTOs.PlayerDTO;
+import com.romm.notlol_api.services.PlayerService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+
+
+
+@RestController @RequestMapping(value = "/")
+
+public class NotLolController {
+
+    @Autowired PlayerService ps;
+
+    @Operation(summary = "Returns the number of days since the player's last match.")
+    @GetMapping("/main")
+    public String main(@RequestBody PlayerDTO data) {
+        String puuid = ps.getPlayerPuid(data.gameName(), data.tagLine());
+
+        return ps.getDaysSinceLastMatch(ps.getPlayerLastMatch(ps.getPlayerMatches(puuid)));
+    }
+    
+}
